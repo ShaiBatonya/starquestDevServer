0b3321502ff879cade09b8ff9406ca7839945abb
Shay_Batonya <127010513+ShaiBatonya@users.noreply.github.com>
2025-07-25 14:04:11 +0300

    fix(cors): update CLIENT_PROD_URL to allow production domain starquest.app

--- src/api/middleware/security/cors.ts
+++ src/api/middleware/security/cors.ts
@@ -4,13 +4,14 @@ import cors from 'cors';
 import { CorsOptions } from 'cors';
 import AppError from '@/api/utils/appError';
 import { vars } from '@/config/vars';
+
 const allowedOrigins = [
-  process.env.CLIENT_PROD_URL,
+  ...process.env.CLIENT_PROD_URL?.split(',') || [],
   process.env.CLIENT_DEV_URL,
   'http://localhost:3000',
   'http://localhost:5173',
   'http://127.0.0.1:6500',
-  'http://localhost:6500'
+  'http://localhost:6500',
 ].filter(Boolean);
 
 const corsOptions: CorsOptions = {
